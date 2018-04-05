2e6334182cb5eb652501633c04de84ae9d5ed791
Unknown <pedicho@gmail.com>
2018-04-05 09:12:29 -0600

    my game

---
+++ Pacman/Assets/Scripts/MoveTrail.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTrail : MonoBehaviour {
+  public int moveSpeed = 230;
+
+  void Update () {
+    transform.Translate (Vector3.right * Time.deltaTime * moveSpeed);
+    Destroy(this.gameObject, 1);
+  }
+
+}
